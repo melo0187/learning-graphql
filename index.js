0afc0d5b97fccb6df7400e721614ddbde98ff9dd
Carmelo Scollo <carmelo.scollo@daimler.com>
2019-03-03 21:10:52 +0100

    Chapter 5: Edges and Connections with many-to-many

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const typeDefs = `
     name: String
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
 
   enum PhotoCategory {
@@ -23,6 +24,7 @@ const typeDefs = `
     description: String
     category: PhotoCategory!
     postedBy: User!
+    taggedUsers: [User!]!
   }
 
   type Query {
@@ -69,6 +71,12 @@ var photos = [
     "githubUser": "sSchmidt"
   }
 ]
+var tags = [
+  { "photoID": "1", "userID": "gPlake"},
+  { "photoID": "2", "userID": "sSchmidt"},
+  { "photoID": "2", "userID": "mHattrup"},
+  { "photoID": "2", "userID": "gPlake"}
+]
 
 const resolvers = {
   Query: {
@@ -89,12 +97,20 @@ const resolvers = {
     url: parent => `http://yoursite.com/img/${parent.id}.jpq`,
     postedBy: parent => {
       return users.find(u => u.githubLogin === parent.githubUser)
-    }
+    },
+    taggedUsers: parent => tags
+      .filter(tag => tag.photoID === parent.id)
+      .map(tag => tag.userID)
+      .map(userID => users.find(u => u.githubLogin === userID))
   },
   User: {
     postedPhotos: parent => {
       return photos.filter(p => p.githubUser === parent.githubLogin)
-    }
+    },
+    inPhotos: parent => tags
+      .filter(tag => tag.userID == parent.githubLogin)
+      .map(tag => tag.photoID)
+      .map(photoID => photos.find(p => p.id === photoID))
   }
 }
 
